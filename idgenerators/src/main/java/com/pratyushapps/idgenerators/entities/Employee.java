291e5acadcab9cd25c41fcd4d65ea911168ff554
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-01-22 10:57:53 +0530

    id custom generator

--- idgenerators/src/main/java/com/pratyushapps/idgenerators/entities/Employee.java
+++ idgenerators/src/main/java/com/pratyushapps/idgenerators/entities/Employee.java
@@ -2,17 +2,18 @@ package com.pratyushapps.idgenerators.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.TableGenerator;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Employee {
 	
-	@TableGenerator(name = "employee_gen", table = "id_gen", pkColumnName = "gen_name", valueColumnName = "gen_val",allocationSize=100)
+	//@TableGenerator(name = "employee_gen", table = "id_gen", pkColumnName = "gen_name", valueColumnName = "gen_val",allocationSize=100)
+	@GenericGenerator(name="emp_id",strategy="com.pratyushapps.idgenerators.CustomRandomIDGenerator")
+	@GeneratedValue(generator="emp_id")
 	@Id
-	@GeneratedValue(strategy  = GenerationType.TABLE,generator = "employee_gen")
-	// @GeneratedValue(strategy = GenerationType.TABLE,generator="employee_gen")
+	//@GeneratedValue(strategy  = GenerationType.TABLE,generator = "employee_gen")
 	private Long id;
 	private String name;
 
