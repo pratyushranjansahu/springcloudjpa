8b90fd7ad8fab14472ee867dadc7216d322ded4e
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-01-22 06:36:09 +0530

    code added for CRUD

---
+++ productdata/src/main/java/com/pratyushapps/productdata/repos/ProductRepository.java
@@ -0,0 +1,27 @@
+package com.pratyushapps.productdata.repos;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.pratyushapps.productdata.entity.Product;
+
+
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+
+    List<Product> findByNameAndDesc(String name, String desc);
+
+    List<Product> findByPriceGreaterThan(Double price);
+
+    List<Product> findByDescContains(String desc);
+
+    List<Product> findByPriceBetween(Double price1, Double price2);
+
+    List<Product> findByDescLike(String desc);
+
+    List<Product> findByIdIn(List<Integer> ids, Pageable pageable);
+}
