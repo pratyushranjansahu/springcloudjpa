1997b671ded55c6af1f133782e41ccd7e36d3998
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-01-27 06:45:34 +0530

    Table per class strategy.

--- hibernateinheritance/src/main/java/com/pratyushapps/hibernateinheritance/entities/Check.java
+++ hibernateinheritance/src/main/java/com/pratyushapps/hibernateinheritance/entities/Check.java
@@ -1,10 +1,10 @@
 package com.pratyushapps.hibernateinheritance.entities;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
-@DiscriminatorValue("ch")
+@Table(name = "bankcheck")
 public class Check extends Payment {
 
 	private String checknumber;
