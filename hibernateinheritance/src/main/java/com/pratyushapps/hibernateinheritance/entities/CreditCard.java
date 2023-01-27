139f6089ea40a104c59a59aa211279f2012817bd
pratyushranjansahu <sahu.ranjan.pratyush@gmail.com>
2023-01-27 06:58:10 +0530

    Joined table strategy.

--- hibernateinheritance/src/main/java/com/pratyushapps/hibernateinheritance/entities/CreditCard.java
+++ hibernateinheritance/src/main/java/com/pratyushapps/hibernateinheritance/entities/CreditCard.java
@@ -1,10 +1,12 @@
 package com.pratyushapps.hibernateinheritance.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "card")
+@PrimaryKeyJoinColumn(name = "id")
 public class CreditCard extends Payment{
 	
 	private String cardnumber;
